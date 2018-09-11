3da5ce4fd1a04b0975b2c79cfd0b1beedacb441d
xypnox <xypnox@gmail.com>
2018-09-11 06:24:33 +0530

    Finished basic homepage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 
         $('.sidenav').sidenav();
         $('.fixed-action-btn').floatingActionButton();
+        $( window ).scroll(function() {
+            $('.fixed-action-btn').css("bottom", "-60px");
+        });
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
