a7e7926e3102b8e10e33833e1fbde617cdac7e8a
xypnox <xypnox@gmail.com>
2018-10-02 23:18:31 +0530

    Added Blogpost Page

---
+++ js/blogpost.js
@@ -0,0 +1,7 @@
+(function($){
+    $(function(){
+
+        $('.sidenav').sidenav();
+        $('.fixed-action-btn').floatingActionButton();
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
