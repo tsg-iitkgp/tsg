4d0c819022ae9e11bb93ab97a74825d42572e222
rka87338 <rka87338@gmail.com>
2019-10-24 23:20:56 +0530

    Added a table which fetches data from the server in events.html page

--- fetchdata.js
+++ fetchdata.js
@@ -1,29 +1,35 @@
-window.onload = getJSON;
-function setQuery() {      // to process the url to be sent to the server
-    const site = "http://gymk-back.herokuapp.com" + window.location.pathname;   
-    let temp = site.split('/');
-    let final = "http://gymk-back.herokuapp.com/" + temp[temp.length - 1];
-    return final;
+// window.onload = getJSON;
+function setQuery() {
+  // to process the url to be sent to the server
+  const site = 'http://gymk-back.herokuapp.com' + events;
+  let temp = site.split('/');
+  let final = 'http://gymk-back.herokuapp.com/' + temp[temp.length - 1];
+  return final;
 }
 
 async function getJSON() {
-    let policy = {
-        mode: 'no-cors'
-    }
-    fetch('http://gymk-back.herokuapp.com/events', policy).then(function (response) {   // url in place of link later
-        console.log(response);
+  let policy = {
+    mode: 'no-cors'
+  };
 
-    }).then(function (data) {
-        console.log(data);   // constructTable(data);
-    }).catch(function (error) {
-        console.log(error);
+  fetch('http://gymk-back.herokuapp.com/events', policy)
+    .then(function(response) {
+      // url in place of link later
+      console.log(response);
+    })
+    .then(function(data) {
+      console.log(data); // constructTable(data);
+    })
+    .catch(function(error) {
+      console.log(error);
     });
 }
+getJSON();
 
 function constructTable() {
-            var data = JSON.stringify('json file here')
+  var data = JSON.stringify('json file here'); //JSON.stringify('json file here');
 
-            /* sample format of JSON file [
+  /* sample format of JSON file [
                 {
                     "Book ID": "1",
                     "Book Name": "Computer Architecture",
@@ -42,38 +48,34 @@ function constructTable() {
                     "Category": "Science",
                     "Price": "210.40"
                 }
-            ] */ //JSON.stringify('json file here');
+            ] */ var heads = [];
 
-            var heads = []
+  for (let i = 0; i < data.length; i++) {
+    // get headings
+    for (var key in data[i]) {
+      if (heads.indexOf(key) === -1) {
+        heads.push(key);
+      }
+    }
+  }
+  var table = document.getElementById('event'); // change id accordingly
 
-            for(let i = 0; i < data.length; i++){  // get headings
-                for(var key in data[i]){
-                    if(heads.indexOf(key) === -1){
-                        heads.push(key);
-                    }
-                }
-            }
-            var table = document.getElementById('event');   // change id accordingly
+  var tr = table.insertRow(-1);
 
-            var tr = table.insertRow(-1);
+  for (let i = 0; i < heads.length; i++) {
+    var th = document.createElement('th');
+    th.innerHTML = heads[i];
+    tr.appendChild(th);
+  }
 
-            for(let i = 0; i < heads.length; i++){
-                var th = document.createElement('th');
-                th.innerHTML = heads[i];
-                tr.appendChild(th);
-            }
-
-            for(let i = 0; i < data.length; i++){
-                tr = table.insertRow(-1);
-                for(let j = 0; j < heads.length; j++){
-
-                    var tabcell = tr.insertCell(-1);
-                    tabcell.innerHTML = data[i][heads[j]];
-                }
-            }
-
-        }
-
+  for (let i = 0; i < data.length; i++) {
+    tr = table.insertRow(-1);
+    for (let j = 0; j < heads.length; j++) {
+      var tabcell = tr.insertCell(-1);
+      tabcell.innerHTML = data[i][heads[j]];
+    }
+  }
+}
 
 /*
 function getJSON() {
